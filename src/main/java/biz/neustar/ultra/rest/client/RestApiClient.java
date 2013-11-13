8dd3b5e19e1a8f76c88a22a76fd14ab3232d4698
Narayan Partangel <narayan.partangel@neustar.biz>
2013-11-13 17:58:19 -0500

    this is the first check in - testing

---
+++ src/main/java/biz/neustar/ultra/rest/client/RestApiClient.java
@@ -0,0 +1,16 @@
+package biz.neustar.ultra.rest.client;
+
+/**
+ * Copyright 2012-2013 NeuStar, Inc. All rights reserved.
+ * NeuStar, the Neustar logo and related names and logos are registered
+ * trademarks, service marks or tradenames of NeuStar, Inc. All other
+ * product names, company names, marks, logos and symbols may be trademarks
+ * of their respective owners.
+ */
+
+public class RestApiClient {
+
+    public static void main (String[] args){
+        System.out.println("first check in");
+    }
+}
