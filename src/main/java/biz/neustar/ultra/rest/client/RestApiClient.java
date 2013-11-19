f93e3141e7afc70998b72cee8fc0a94c7339a477
Jon Bodner <jonathan.bodner@neustar.biz>
2013-11-19 18:43:23 -0500

    get build.gradle working, very early skeleton for RestApiClient

--- src/main/java/biz/neustar/ultra/rest/client/RestApiClient.java
+++ src/main/java/biz/neustar/ultra/rest/client/RestApiClient.java
@@ -1,5 +1,7 @@
 package biz.neustar.ultra.rest.client;
 
+import biz.neustar.ultra.rest.main.UltraRestClient;
+
 /**
  * Copyright 2012-2013 NeuStar, Inc. All rights reserved.
  * NeuStar, the Neustar logo and related names and logos are registered
@@ -9,8 +11,18 @@ package biz.neustar.ultra.rest.client;
  */
 
 public class RestApiClient {
+    private final UltraRestClient _ultraRestClient;
 
     public static void main (String[] args){
         System.out.println("first check in");
+
+    }
+
+    public RestApiClient(String userName, String password, String url, String authUrl) {
+        _ultraRestClient = UltraRestClient.createRestClientNoAuth(url);
+    }
+
+    public String getVersion() {
+        return _ultraRestClient.get("/v1/version").getBody();
     }
 }
